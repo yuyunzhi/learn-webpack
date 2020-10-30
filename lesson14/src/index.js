6363f0359ca86c1c230929cc5002cb312e21ced6
yuyunzhi <lambert1129@foxmail.com>
2020-10-30 16:10:26 +0800

    feat: 🎸 add lazy loading

---
+++ lesson14/src/index.js
@@ -0,0 +1,19 @@
+import _ from "lodash";  // 假设有1MB
+
+// let element = document.createElement('div')
+// element.innerHTML = _.join(['Dell', 'Lee'], '-');
+// document.body.appendChild(element)
+
+// 懒加载
+async function getComponent() {
+  const {default: _} = await import(/* webpackChunkName:"lodash" */'lodash')
+  const element = document.createElement('div')
+  element.innerHTML = _.join(['y', 'kk'], '-');
+  return element;
+}
+
+document.addEventListener('click', () => {
+  getComponent().then(element => {
+    document.body.appendChild(element)
+  })
+})
