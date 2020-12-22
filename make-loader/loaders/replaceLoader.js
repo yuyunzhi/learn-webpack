0ee8815f1cdab4a7e0d8fa26fe31a46bf0712f85
yuyunzhi <lambert1129@foxmail.com>
2020-12-22 16:36:24 +0800

    docs: ✏️ 添加注释

--- make-loader/loaders/replaceLoader.js
+++ make-loader/loaders/replaceLoader.js
@@ -3,6 +3,7 @@
 // 同步loader
 module.exports = function (source) {
   console.log('source',source);
+  //对source做抽象语法树处理，来修改源代码
   console.log('this.query',this.query) // {name:yuyunzhi}
   return source.replace('ykk','yuyunzhi')
 }
